d2431793b0d89b33c2b1e7def07f11dd12b79544
marcin_html <marcinszlaz@gmail.com>
2026-04-09 16:32:20 +0200

    push me pull me

---
+++ 134/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function()
+{
+    $(".zakladka").click(function()
+    {
+        $(this).toggleClass("zakladka_clicked");
+    });
+});
---
+++ 135/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function()
+{
+   $("#stopResumeAnimation").click(function()
+   {
+       $(".zakladka").toggleClass("stopAnimation");
+   });
+});
